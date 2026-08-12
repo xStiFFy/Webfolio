504b99a19e6f01bc53c27ed6a6fb66a94407a763
Bad_Mitches <mitchell.gegich@gmail.com>
2026-08-12 19:25:55 -0400

    Add hero and background effects

---
+++ src/components/Hero.tsx
@@ -0,0 +1,68 @@
+import './Hero.css'
+import monogram_purple from '../assets/monogram_purple.svg'
+import GradientWaves from './effects/GradientWaves';
+
+function Hero() {
+
+    return (
+        <section className="hero" id="home">   
+
+            <div className="hero-background">
+                <GradientWaves
+                    horizonColor="#5227FF"
+                    waveColor="#FF9FFC"
+                    crestColor="#FFFFFF"
+                    speed={.5}
+                    amplitude={2.5}
+                    waveScale={1}
+                    waveRatio={0.9}
+                    swell={35}
+                    turbulence={20}
+                    tilt={1.11}
+                    zoom={1.85}
+                    height={5.5}
+                    fogDepth={15}
+                    detail="medium"
+                    brightness={1}
+                    opacity={1}
+                    mouseInteraction={false}
+                    parallaxStrength={0.5}
+                    grain
+                    grainIntensity={0.05}
+                />
+            </div>
+
+            <div className="hero-content">
+                <h3 className="hero-intro">Hello, I'm</h3>
+
+                <h1 className="hero-name">
+                    Mitchell Gegich
+                </h1>
+
+                <h2 className="hero-title">
+                    Software Developer
+                </h2>
+
+                <p className="hero-description">
+                    {/* Short professional introduction */}
+                </p>
+
+                <div className="hero-actions">
+                    <a href="#projects" className="hero-button hero-button-primary glow-hover">
+                        View Projects
+                    </a>
+
+                    <a href="/resume.pdf" className="hero-button hero-button-secondary glow-hover">
+                        View Resume
+                    </a>
+                </div>
+            </div>
+
+            <div className="hero-visual">
+                <img src={monogram_purple} className="hero-monogram"alt="MG" />
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,43 @@
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import WebThreads from '../components/effects/WebThreads'
 
 function Homepage() {
 
     return (
         <>
-            <Navbar />
+            <div className="website-background">
+                <WebThreads
+                    color1="#5227FF"
+                    color2="#ef3cff"
+                    color3="#FFFFFF"
+                    speed={0.02}
+                    threadCount={6}
+                    frequency={5}
+                    spread={0.6}
+                    taper={.35}
+                    position={0.5}
+                    fanMode="center"
+                    glow={0.014}
+                    falloff={0.5}
+                    thickness={.8}
+                    brightness={0.6}
+                    opacity={1}
+                    mirror
+                    shimmer={false}
+                    grain={false}
+                    grainIntensity={0.05}
+                    mouseInteraction={false}
+                    mouseStrength={0.3}
+                />
+            </div>
+
+            <div className="website-foreground">
+                <Navbar />
+
+                <Hero />
+
+            </div>
         </>
     )
 }
